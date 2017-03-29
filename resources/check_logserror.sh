e1f13d89b68e19593d11e1b407694f244f84d23a
Cédric Dupont <lunarok@gmail.com>
2017-03-29 20:45:25 +0200

    output check

--- resources/check_logserror.sh
+++ resources/check_logserror.sh
@@ -13,7 +13,7 @@ if [ $(grep "ERROR" -R $DIRECTORY | grep ' . date('Y-m-d', time()) . ' | wc -l )
     EXIT_STRING=$(grep "ERROR" -R $DIRECTORY | grep ' . date('Y-m-d', time()) . ' | tr -d '\n')
     EXIT_CODE=2
 else
-    EXIT_STRING=`Logs Jeedom OK`
+    EXIT_STRING="Backup find"
     EXIT_CODE=0
 fi
 
