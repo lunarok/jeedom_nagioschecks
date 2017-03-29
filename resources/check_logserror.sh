17de43e7fe8dfe620a5a347fb06314545742053a
Cédric Dupont <lunarok@gmail.com>
2017-03-29 12:52:11 +0200

    check

--- resources/check_logserror.sh
+++ resources/check_logserror.sh
@@ -10,7 +10,7 @@ do
 done
 
 if [ $(grep "ERROR" -R $DIRECTORY | grep ' . date('Y-m-d', time()) . ' | wc -l ) -gt 0 ]; then
-    EXIT_STRING="No error in logs"
+    EXIT_STRING=`grep "ERROR" -R $DIRECTORY | grep ' . date('Y-m-d', time()) . ' | tr -d '\n'`
     EXIT_CODE=0
 else
     EXIT_STRING="Error in logs"
