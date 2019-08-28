52bd877b931af833cfdeec5cac71f4a74df2f31e
Cédric Dupont <lunarok@gmail.com>
2019-08-28 14:37:21 +0200

    Update nagioschecks.php

--- desktop/php/nagioschecks.php
+++ desktop/php/nagioschecks.php
@@ -38,7 +38,7 @@ $eqLogics = eqLogic::byType('nagioschecks');
                 $opacity = ($eqLogic->getIsEnable()) ? '' : jeedom::getConfiguration('eqLogic:style:noactive');
                 echo '<div class="eqLogicDisplayCard cursor" data-eqLogic_id="' . $eqLogic->getId() . '" style="background-color : #ffffff ; height : 200px;margin-bottom : 10px;padding : 5px;border-radius: 2px;width : 160px;margin-left : 10px;' . $opacity . '" >';
                 echo "<center>";
-                echo '<img src="plugins/nagioschecks/doc/images/nagioschecks_icon.png" height="105" width="95" />';
+                echo '<img src="plugins/nagioschecks/plugin_info/nagioschecks_icon.png" height="105" width="95" />';
                 echo "</center>";
                 echo '<span style="font-size : 1.1em;position:relative; top : 15px;word-break: break-all;white-space: pre-wrap;word-wrap: break-word;"><center>' . $eqLogic->getHumanName(true, true) . '</center></span>';
                 echo '</div>';
